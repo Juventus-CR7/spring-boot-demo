b520bab813e5ec0ed85725d2bcf64ab92aedd172
Juventus-CR7 <juventus_pd@163.com>
2020-12-02 22:10:24 +0800

    测试文件爆红

---
+++ spring-boot-demo-configuration/src/main/java/com/lys/config/FoodConfig.java
@@ -0,0 +1,5 @@
+package com.lys.config;
+
+
+public class FoodConfig {
+}
---
+++ spring-boot-demo-configuration/src/main/java/com/lys/domain/Food.java
@@ -0,0 +1,14 @@
+package com.lys.domain;
+
+
+public class Food {
+    private String rice;
+
+    public String getRice() {
+        return rice;
+    }
+
+    public void setRice(String rice) {
+        this.rice = rice;
+    }
+}
